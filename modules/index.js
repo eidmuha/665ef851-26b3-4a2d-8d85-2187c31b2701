df22ae4e663fbbfb832386064f9d7fecd97790f4
Eid Muhammad <eid.muhammad@ibuildnew.com.au>
2023-11-25 15:13:01 +1100

    New: implement progress report.

--- modules/index.js
+++ modules/index.js
@@ -25,12 +25,13 @@ const app = () => {
     const strands = questionsData.map((item) => item.strand);
     const strandsCount = countUnique(strands)
     const student = studentsData.find(st => st.id === studentId);
+    const studentFullName = `${student.firstName} ${student.lastName}`;
 
-    const generateProgressReport = (selectedStudent, assessment) => {
+    function getLatestAssessment(assessment) {
         let latestAssessment = null;
         studentResponsesData.filter(sResponse => {
             // getting the latest assessment
-            if (sResponse.student.id === studentId && sResponse.assessmentId === assessment.id) {
+            if (sResponse.student.id === studentId && sResponse.assessmentId === assessment.id && sResponse.completed) {
                 if (latestAssessment && sResponse.student.yearLevel) {
                     // get latest response based on the last year
                     if (sResponse.student.yearLevel > latestAssessment.student.yearLevel) {
@@ -41,11 +42,16 @@ const app = () => {
                 }
             }
         });
+        return latestAssessment;
+    }
 
-        // console.log(latestAssessment);
+    // Case 1
+    const generateDiagnosticReport = (assessment) => {
+        let latestAssessment = getLatestAssessment(assessment);
+
         if (latestAssessment) {
             const date = moment(latestAssessment.completed, "DD.MM.YYYY hh.mm.ss").format("Do MMMM gggg LT");
-            console.log(`${selectedStudent.firstName} ${selectedStudent.lastName} has completed ${assessment.name} assessment on ${date}`);
+            console.log(`${studentFullName} has completed ${assessment.name} assessment on ${date}`);
             console.info(`He got ${latestAssessment.results.rawScore} questions right out of ${questionsData.length}. Details by strand given bellow\n`);
 
             for (const key in strandsCount) {
@@ -70,20 +76,56 @@ const app = () => {
 
     };
 
+    // Case 2
+    const generateProgressReport = (assessment) => {
+        const tsAssessments = studentResponsesData.filter(ass => ass.student.id === student.id && ass.completed);
+
+        const subTitle = `${studentFullName} has completed ${assessment.name} assessment ${tsAssessments.length}`;
+        console.log(`\n${subTitle} times in total. Date and raw score given below:\n`);
+
+        let rawScores = [];
+        tsAssessments.forEach(tsAssessment => {
+            const tDate = `Date: ${ moment(tsAssessment.assigned, "DD.MM.YYYY hh.mm.ss").format("Do MMMM gggg")}`;
+            const tScore = `Raw Score: ${tsAssessment.results.rawScore} out of ${tsAssessment.responses.length}`;
+            console.info(`${tDate}, ${tScore}`);
+            rawScores.push(tsAssessment.results.rawScore);
+        });
+
+        console.log("");
+
+        const minScore = Math.min(...rawScores);
+        const maxScore = Math.max(...rawScores);
+        const diff = maxScore - minScore;
+        if (diff > 0) {
+            console.info(`${studentFullName} got ${diff} more correct in the recent completed assessment than the oldest`);
+        } else if (diff < 0) {
+            console.info(`${studentFullName} got ${minScore - maxScore} less correct in the recent completed assessment than the oldest`);
+        } else {
+            console.info(`${studentFullName} got same correct in the recent completed assessment than the oldest`);
+        }
+
+        console.log("");
+
+    };
+
+
+
     assessmentsData.forEach(assessment => {
         switch (reportType) {
             case "1":
                 console.log("\n============= Diagnostic Report =============");
-                generateProgressReport(student, assessment);
+                generateDiagnosticReport(assessment);
                 break;
             case "2":
-                console.log("============= Progress Report =============");
+                console.log("\n============= Progress Report =============");
+                generateProgressReport(assessment);
                 break;
             case "3":
-                console.log("============= Feedback Report =============");
+                console.log("\n============= Feedback Report =============");
+                // generateFeedbackReport(assessment);
                 break;
             default:
-                console.log("No report generated. Please run the app again and enter report to generate!");
+                console.log("\nNo report generated. Please run the app again and enter report to generate!\n");
         }
     });
 
